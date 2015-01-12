2655b68912715831f885024ce96e2a9481a10a6e
vsuperator <roma9209@gmail.com>
2015-01-12 14:40:38 +0200

    gulp: jade compile added

--- builder/gulpfile.js
+++ builder/gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
     opn = require('opn'),
     notify = require('gulp-notify'),
     sass = require('gulp-sass'),
+    jade = require('gulp-jade'),
     prefix = require('gulp-autoprefixer'),
     minify = require('gulp-minify-css'),
     plumber = require('gulp-plumber'),
@@ -18,6 +19,7 @@ gulp.task('connect', function() {
     });
     opn('http://localhost:8080/');
 })
+
 // Compile scss to css
 
 gulp.task('css', function() {
@@ -33,8 +35,22 @@ gulp.task('css', function() {
     	.pipe(notify('Css compiled'));
 })
 
+// Compile Jade to HTML
+
+gulp.task('html', function() {
+    gulp.src('../_dev/_makeups/*.jade')
+        .pipe(plumber({errorHandler: notify.onError("Error: HTML")}))
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('../public/'))
+        .pipe(connect.reload())
+        .pipe(notify('HTML compiled'));
+})
+
 gulp.task('watch', function() {
-	gulp.watch('../_dev/_styles/**/*.scss', ['css']);
+    gulp.watch('../_dev/_styles/**/*.scss', ['css']);
+	gulp.watch('../_dev/_makeups/**/*.jade', ['html']);
 })
 
 gulp.task('default', ['connect', 'watch']);
